051ff8628fe3f643b40fcce7a4ea68856a442b1a
DevPrudent <devprudent@gmail.com>
2025-06-09 18:22:10 +0100

    Updated NewRegistrationPage.tsx

--- src/pages/NewRegistrationPage.tsx
+++ src/pages/NewRegistrationPage.tsx
@@ -1,13 +1,7 @@
 import MultiStepRegistration from "@/components/registration/MultiStepRegistration";
 
 const NewRegistrationPage = () => {
-  return (
-    <>
-      {/* Add Paystack script to the page */}
-      <script src="https://js.paystack.co/v1/inline.js"></script>
-      <MultiStepRegistration />
-    </>
-  );
+  return <MultiStepRegistration />;
 };
 
 export default NewRegistrationPage;
